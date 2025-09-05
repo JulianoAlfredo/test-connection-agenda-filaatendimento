8df0b8504ad50030a8156ab099f9104676981cca
JulianoAlfredo <juliano.alfredo86@gmail.com>
2025-09-05 16:11:14 -0300

    added a cors on middleware for all routes

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ app.use(
   })
 )
 
-// Middleware para adicionar cabeçalhos CORS manualmente
+//adiciona pois o frontend estava com erro no cors ao consumir a API
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*')
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
